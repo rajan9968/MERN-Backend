67314b89a8b597e34c7eedb9cc435f13d03334d7
Rajan <ra580jan@gmail.com>
2025-03-23 22:35:25 +0530

    Add WrokAssign

--- Routes/AdminRoutes/WorkAssignRoutes.js
+++ Routes/AdminRoutes/WorkAssignRoutes.js
@@ -1,12 +1,6 @@
 const router = require('express').Router();
 const { assignedwork } = require('../../Controller/Admin/WorkAssignController');
 
-// router.post('/assignedwork', assignedwork);
-
-router.post('/assignedwork', (req, res) => {
-    console.log("Route hit!");
-    console.log("Body:", req.body);  // Log the request body
-    res.status(200).send("Route is working!");
-});
+router.post('/assignedwork', assignedwork);
 
 module.exports = router;
