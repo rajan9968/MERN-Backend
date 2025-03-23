fdfd2c7d410ad3889a54fdbd95c3dec1d73a50a4
Rajan <ra580jan@gmail.com>
2025-03-23 21:23:48 +0530

    Add WrokAssign

--- Routes/AdminRoutes/WorkAssignRoutes.js
+++ Routes/AdminRoutes/WorkAssignRoutes.js
@@ -1,6 +1,6 @@
 const router = require('express').Router();
 const { assignedwork } = require('../../Controller/Admin/WorkAssignController');
 
-router.post('/work', assignedwork);
+router.post('/assignedwork', assignedwork);
 
 module.exports = router;
